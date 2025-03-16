6f8a12041afe204f44a00be070b6a1f6c501acb2
ashhssn <ashsyahidhussin@gmail.com>
2025-03-16 18:51:47 +0800

    feat: Process image input

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import os
 import time
 from modules.agent import InfauxAgent
+from modules.paddle_ocr import extract_text_from_image
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Replace with a strong secret key
@@ -27,14 +28,12 @@ def user_query():
         text_message = request.form.get("user_text_input", "").strip() 
         uploaded_image = request.files.get("uploaded_image")  
         is_uploaded_image_exists = uploaded_image and uploaded_image.filename != "" 
-        # print("Text:", text_message)
-        # print("Uploaded Image:", uploaded_image.filename if is_uploaded_image_exists else "No Image Uploaded")
-        if text_message and is_uploaded_image_exists:
+        if is_uploaded_image_exists:
             filename = save_image(uploaded_image)
-            # print("User Entered Text And Uploaded Image") Format 1
+            text_message = extract_text_from_image(filename)
+            response = agent.respond(text_message)
             return jsonify({"success": "User Entered Text And Uploaded Image",
-                            "response" : "User Entered Text And Uploaded Image",
-                            "uploaded_image_url" : filename
+                            "response" : response
                             }), 200
         
         if text_message and not is_uploaded_image_exists:
